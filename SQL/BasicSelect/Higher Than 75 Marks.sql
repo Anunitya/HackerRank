1dc95f7973161580628c6cfe216f20e7189953d4
Anunitya <32194063+Anunitya@users.noreply.github.com>
2021-02-25 12:06:25 +0530

    Create Higher Than 75 Marks.sql

---
+++ SQL/BasicSelect/Higher Than 75 Marks.sql
@@ -0,0 +1,14 @@
+SELECT NAME 
+FROM STUDENTS
+WHERE MARKS > 75 
+      ORDER BY RIGHT(NAME,3), ID
+      
+      
+      /*
+      
+SELECT NAME 
+FROM STUDENTS
+WHERE MARKS > 75
+      ORDER BY SUBSTR(NAME,-3,3) , ID
+      
+      */
