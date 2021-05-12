28d8275cb180769e5a61234c0ac26044c0f351ba
Anunitya <32194063+Anunitya@users.noreply.github.com>
2021-05-12 13:36:18 +0530

    Update Maximum Element.py

--- DataStructures/stack/Maximum Element.py
+++ DataStructures/stack/Maximum Element.py
@@ -13,6 +13,8 @@ import sys
 # The function accepts STRING_ARRAY operations as parameter.
 #
 
+'''
+#This takes too much time 
 def getMax(operations):
     stack = []
     final = []
@@ -25,8 +27,25 @@ def getMax(operations):
         elif i[0] == '3':
             final.append(max(stack))
     return final
-             
-            
+    
+ '''        
+'''
+
+We do not need to maintain two stacks. 
+Instead, we can use a single stack of integers: For query (1), instead of pushing element X, we simply push max(x, stack.peek()). 
+    For query (2), simply pop an element. For query (3), print stack.peek().
+In a nutshell, we don't care that X is at the top of the stack if there is an element Y > X further down in the stack, so why not just push Y?
+            '''
+
+def getMax(operations):
+    items = [0]
+    final = []
+    for i in operations:
+        x= list(map(int,i.split(" ")))
+        if x[0] == 1 : items.append(max(x[1],items[-1]))
+        elif x[0] == 2 : items.pop()
+        else: final.append(items[-1])
+    return final
         
 
 if __name__ == '__main__':
